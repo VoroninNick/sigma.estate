76fbda256467b562b5a2364230767ffc612bda2a
Nazariy <nazariy.papizh@gmail.com>
2015-08-20 18:31:53 +0300

    added redirect after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   before_action :setup_mcapi
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  after_filter :store_location
 
   def setup_mcapi
     @mc = Mailchimp::API.new('aa3920184d9c25995efadfdda3f13803-us11')
@@ -18,4 +19,16 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :subscribe) }
       devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :avatar, :subscribe) }
     end
+
+
+
+
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
 end
