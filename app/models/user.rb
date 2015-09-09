56b89772ab10c37d260cf646ab891227b64aa7a7
Nazariy <nazar@Nazars-iMac.local>
2015-09-09 11:33:53 +0300

    change self tabel name for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  # self.table_name = :users
+  self.table_name = :sigma_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
