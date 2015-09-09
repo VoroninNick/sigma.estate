b11b255cf0aae23b60eca724accfbf691e22dea4
Nazariy <nazar@Nazars-iMac.local>
2015-09-09 11:48:51 +0300

    fixed user model and view forms

--- app/models/sigma/user.rb
+++ app/models/sigma/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class Sigma::User < ActiveRecord::Base
   self.table_name = :sigma_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
 
-  devise_for :users, controllers: {registrations: 'registrations'}
+  devise_for :users, class_name: "Sigma::User", controllers: {registrations: 'registrations'}
 
 
   # devise_for :users, controllers: {
