f04b939debc24daefbba098dfd0b24fb4f5ada07
Nazariy <nazar@Nazars-iMac.local>
2015-09-18 11:23:00 +0300

    add more email to mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -7,13 +7,13 @@ class ContactMailer < ApplicationMailer
     @email = email
     @message = message
 
-    mail(:template_path => 'contact_mailer', :template_name => 'have_questions', layout: false,from: email, subject: 'Виникли питання', to: SupportEmail.first.have_questions)
+    mail(:template_path => 'contact_mailer', :template_name => 'have_questions', layout: false,from: email, subject: 'Виникли питання', to: SupportEmail.first.have_questions.split(','))
   end
 
   def call_to_order(user_name, phone_number)
     @user_name = user_name
     @phone_number = phone_number
 
-    mail(:template_path => 'contact_mailer', :template_name => 'call_to_order', :layout => false, :subject => 'Замовити дзвінок', :to => SupportEmail.first.call_to_order)
+    mail(:template_path => 'contact_mailer', :template_name => 'call_to_order', :layout => false, :subject => 'Замовити дзвінок', :to => SupportEmail.first.call_to_order.split(','))
   end
 end
